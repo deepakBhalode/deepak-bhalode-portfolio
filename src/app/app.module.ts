cdd363abbef9de27155385580ff115557ccdc5d0
deepakBhalode <deepak.bhalode.sbg@gmail.com>
2022-01-22 20:30:49 +0530

    initial structure created

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { SideNavigationComponent } from './components/side-navigation/side-navigation.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IntroductionComponent,
+    SideNavigationComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
