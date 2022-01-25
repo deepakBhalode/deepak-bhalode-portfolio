8fc71ac7fc955489c3b10a03dda1232a3b72df40
deepakBhalode <deepak.bhalode.sbg@gmail.com>
2022-01-25 14:01:59 +0530

    blog component completed

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { latestBlogsList } from 'src/app/shared/resources/about-content-list';
 
 @Component({
   selector: 'app-blog',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+
+  latestBlogsDetailList : any = latestBlogsList;
 
   constructor() { }
 
